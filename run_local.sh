2033f726b3f89aa9cdacc63c7a5777098677b2ed
Rooke Poole <rookpool97@gmail.com>
2026-07-25 11:12:17 -0500

    Delete run_local.sh

--- run_local.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-cd "$(dirname "$0")"
-echo "Starting ForgeTrace at http://127.0.0.1:8765"
-echo "Repository files are stored in: $(pwd)/workspace"
-python3 server.py --port 8765
